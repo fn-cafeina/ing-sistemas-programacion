681a2ed5d33b6ff77c68bbe4edefa39ef0d93a3e
fn-cafeina <201848984+fn-cafeina@users.noreply.github.com>
2025-03-24 14:28:18 -0600

    pequeñas correcciones

--- programacion-estructurada/lab-programacion-estructurada/5/sexo.c
+++ programacion-estructurada/lab-programacion-estructurada/5/sexo.c
@@ -3,21 +3,25 @@
 int main()
 {
         int sexo;
+        char nombre[99];
+
+        printf("Ingrese su nombre: ");
+        scanf("%s", &nombre);
 
         printf("Ingrese su sexo: \n0 - Masculino \n1 - Femenino \n2 - Otro \n");
         scanf("%d", &sexo);
 
         if (sexo == 0)
         {
-                printf("Usted es hombre. \n");
+                printf("Su nombres es: %s, y su sexo es: Masculino \n", nombre);
         }
         else if (sexo == 1)
         {
-                printf("Usted es mujer. \n");
+                printf("Su nombres es: %s, y su sexo es: Femenino \n", nombre);
         }
         else
         {
-                printf("Usted es de otro genero. \n");
+                printf("Su nombres es: %s, y usted es de otro genero \n");
         }
 
         return 0;
