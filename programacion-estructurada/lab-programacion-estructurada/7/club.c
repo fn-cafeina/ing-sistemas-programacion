7163a06af4a312dfc569abb6fadc84d3577e753f
fn-cafeina <201848984+fn-cafeina@users.noreply.github.com>
2025-04-01 10:24:26 -0600

    corrección

--- programacion-estructurada/lab-programacion-estructurada/7/club.c
+++ programacion-estructurada/lab-programacion-estructurada/7/club.c
@@ -3,10 +3,13 @@
 // mayores de 18 años o a personas menores de 16 años acompañadas por un adulto.
 
 #include <stdio.h>
+#include <ctype.h>
+#include <string.h>
 
 int main()
 {
         int edad;
+        char opcion[3];
 
         printf("Ingrese su edad para entrar al club: ");
         scanf("%d", &edad);
@@ -15,7 +18,11 @@ int main()
                 printf("Usted puede entrar al club.\n");
         else
         {
-                printf("Usted no puede entrar al club.\n");
+                printf("Viene acompañado de un adulto?\nR = ");
+                scanf("%s", &opcion);
+
+                if (strcmp(opcion, "Si") == 0 || strcmp(opcion, "si") == 0)
+                        printf("\nUsted puede entrar al club.\n");
         }
 
         return 0;
