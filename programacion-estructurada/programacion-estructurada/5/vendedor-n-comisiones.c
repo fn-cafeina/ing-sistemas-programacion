3ac78b574fd6e86f3bad3db66fc761149acf724b
fn-cafeina <201848984+fn-cafeina@users.noreply.github.com>
2025-04-21 17:26:48 -0600

    nuevo ejercicio

---
+++ programacion-estructurada/programacion-estructurada/5/vendedor-n-comisiones.c
@@ -0,0 +1,74 @@
+// Un vendedor desea calcular la comisión total sobre la venta de varios artículos. Al vendedor
+// le corresponde el 10% de comisión sobre artículos, cuyo precio es menor o igual a $1000 y el
+// 7% de comisión sobre aquellos artículos cuyo precio sea mayor de $1000. Elabore un
+// programa para obtener la comisión total, si se conoce que el vendedor hizo N ventas.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void limpiarBuffer()
+{
+        while (getchar() != '\n')
+                ;
+}
+
+int main()
+{
+        if (system("cls"))
+                system("clear");
+
+        int nVentas;
+        int esValido;
+        float valorArticulo, pagoComisiones;
+
+        printf("Ingrese la cantidad de ventas: ");
+        esValido = scanf("%d", &nVentas);
+
+        if (esValido == 1)
+        {
+                if (nVentas <= 0)
+                {
+                        printf("\nIngrese un numero de ventas positivos.\n");
+                        return 1;
+                }
+        }
+        else
+        {
+                printf("\nIngrese un tipo de dato valido.\n");
+                return 1;
+        }
+
+        int i;
+
+        for (i = 1; i <= nVentas; i++)
+        {
+                printf("\nIngrese el precio del articulo vendido #%d: $", i);
+                esValido = scanf("%f", &valorArticulo);
+
+                if (esValido == 1)
+                {
+                        if (valorArticulo <= 0)
+                        {
+                                printf("\nAl parecer regaló un articulo, esto afectara su pago.\n");
+                                limpiarBuffer();
+                                continue;
+                        }
+
+                        if (valorArticulo <= 1000)
+                                pagoComisiones += valorArticulo * 0.1;
+                        else
+                                pagoComisiones += valorArticulo * 0.07;
+                }
+                else
+                {
+                        printf("\nIngrese un tipo de dato valido, volviendo a iterar...\n");
+                        limpiarBuffer();
+                        i--;
+                        continue;
+                }
+        }
+
+        printf("\nEl pago total de las comisiones de %d ventas es de: $%.2f\n", nVentas, pagoComisiones);
+
+        return 0;
+}
