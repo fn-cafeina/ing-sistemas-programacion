07907bd2831a6ec6359554c9b3321e916b3347b4
fn-cafeina <201848984+fn-cafeina@users.noreply.github.com>
2025-04-23 15:13:07 -0600

    nuevos ejercicios

---
+++ programacion-estructurada/programacion-estructurada/5/empleado-sueldos-while.c
@@ -0,0 +1,38 @@
+// Construya un programa que, al recibir como datos la clave del empleado y los seis
+// primeros sueldos del año, calcule el ingreso total semestral y el promedio mensual, e
+// imprima la clave del empleado, el ingreso total y el promedio mensual.
+
+#include <stdio.h>
+
+int main()
+{
+
+        int claveEmpleado;
+
+        printf("Ingrese la clave del empleado: ");
+        scanf("%d", &claveEmpleado);
+
+        float ingresoTotal;
+
+        int i;
+
+        while (i <= 6)
+        {
+                float sueldo;
+
+                printf("\nIngrese el sueldo #%d: ", i);
+                scanf("%f", &sueldo);
+
+                ingresoTotal += sueldo;
+
+                i++;
+        }
+
+        float promedioMensual = ingresoTotal / 12;
+
+        printf("\nClave del empleado: %d\n", claveEmpleado);
+        printf("Ingreso total del empleado: %.2f\n", ingresoTotal);
+        printf("Promedio mensual: %.2f\n", promedioMensual);
+
+        return 0;
+}
