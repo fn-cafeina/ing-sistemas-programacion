6828987e7baa762cd45e8d7049caf015bb1f251b
fn-cafeina <201848984+fn-cafeina@users.noreply.github.com>
2025-05-08 12:58:49 -0600

    cambios en la libreria

--- programacion-estructurada/utilidades/cafeina.h
+++ programacion-estructurada/utilidades/cafeina.h
@@ -1,10 +1,10 @@
 #ifndef CAFEINA_H
 #define CAFEINA_H
 
-int sumar(int a, int b);
-int restar(int a, int b);
-int multiplicar(int a, int b);
-int esFibonacci(int n);
+int sumar(int, int);
+int restar(int, int);
+int multiplicar(int, int);
+int esFibonacci(int);
 void limpiarBuffer(void);
 void limpiarConsola(void);
 
