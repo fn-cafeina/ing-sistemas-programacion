982a66254983d42db5506686cd9180fa492b8905
fn-cafeina <201848984+fn-cafeina@users.noreply.github.com>
2025-05-14 14:29:55 -0600

    chkpt

---
+++ programacion-estructurada/programacion-estructurada/10/intercambio-de-filas.c
@@ -0,0 +1,50 @@
+// Realice un programa en C que intercambie las M filas de un
+// arreglo bidimensional. Los elementos de la primera fila se
+// intercambian con los de la última fila, los de la segunda con
+// los de la penúltima y así sucesivamente.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void imprimirMatriz(int **, int, int);
+
+int main()
+{
+        int tamanyioFilas, tamanyioColumnas;
+
+        printf("Ingrese la cantidad de filas: ");
+        scanf("%d", &tamanyioFilas);
+
+        printf("Ingrese la cantidad de columnas: ");
+        scanf("%d", &tamanyioColumnas);
+
+        int **arreglo;
+        arreglo = (int **)malloc((size_t)tamanyioFilas * sizeof(int *));
+
+        for (int i = 0; i < tamanyioFilas; i++)
+                arreglo[i] = (int *)malloc((size_t)tamanyioColumnas * sizeof(int));
+
+        printf("\n");
+
+        for (int i = 0; i < tamanyioFilas; i++)
+        {
+                for (int j = 0; j < tamanyioColumnas; j++)
+                {
+                        printf("Ingrese el valor de la posición [%d][%d]: ", i, j);
+                        scanf("%d", &arreglo[i][j]);
+                }
+        }
+
+        imprimirMatriz(arreglo, tamanyioFilas, tamanyioColumnas);
+
+        return 0;
+}
+
+void imprimirMatriz(int **arreglo, int tamanyioFilas, int tamanyioColumnas)
+{
+        for (int i = 0; i < tamanyioFilas; i++)
+        {
+                for (int j = 0; j < tamanyioColumnas; j++)
+                        printf("[%d]\n", arreglo[i][j]);
+        }
+}
