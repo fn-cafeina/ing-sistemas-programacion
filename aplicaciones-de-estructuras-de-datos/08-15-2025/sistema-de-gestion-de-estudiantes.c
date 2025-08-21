2e24e04e438ded651df43d9ab7a5b6c17de0d7ee
Jasmir Medina <201848984+fn-cafeina@users.noreply.github.com>
2025-08-21 09:42:53 -0600

    mejora la indentación y el formato del código en el programa de gestión de estudiantes

--- aplicaciones-de-estructuras-de-datos/08-15-2025/sistema-de-gestion-de-estudiantes.c
+++ aplicaciones-de-estructuras-de-datos/08-15-2025/sistema-de-gestion-de-estudiantes.c
@@ -17,60 +17,60 @@
 
 struct Estudiante
 {
-  char nombre[30];
-  char correo[50];
-  int edad;
-  float promedio;
+        char nombre[30];
+        char correo[50];
+        int edad;
+        float promedio;
 };
 
 int main()
 {
-  struct Estudiante estudiantes[N_ESTUDIANTES];
+        struct Estudiante estudiantes[N_ESTUDIANTES];
 
-  for (size_t i = 0; i < N_ESTUDIANTES; i++)
-  {
-    printf("\nRegistro estudiante #%d\n", i + 1);
+        for (size_t i = 0; i < N_ESTUDIANTES; i++)
+        {
+                printf("\nRegistro estudiante #%d\n", i + 1);
 
-    printf("Nombre: ");
-    scanf("%s", estudiantes[i].nombre);
+                printf("Nombre: ");
+                scanf("%s", estudiantes[i].nombre);
 
-    printf("Correo: ");
-    scanf("%s", estudiantes[i].correo);
+                printf("Correo: ");
+                scanf("%s", estudiantes[i].correo);
 
-    printf("Edad: ");
-    scanf("%d", &estudiantes[i].edad);
+                printf("Edad: ");
+                scanf("%d", &estudiantes[i].edad);
 
-    float nota1, nota2, nota3;
+                float nota1, nota2, nota3;
 
-    printf("Ingrese la nota de Calculo: ");
-    scanf("%f", &nota1);
+                printf("Ingrese la nota de Calculo: ");
+                scanf("%f", &nota1);
 
-    printf("Ingrese la nota de Fisica: ");
-    scanf("%f", &nota2);
+                printf("Ingrese la nota de Fisica: ");
+                scanf("%f", &nota2);
 
-    printf("Ingrese la nota de Programacion: ");
-    scanf("%f", &nota3);
+                printf("Ingrese la nota de Programacion: ");
+                scanf("%f", &nota3);
 
-    estudiantes[i].promedio = (nota1 + nota2 + nota3) / 3;
-  }
+                estudiantes[i].promedio = (nota1 + nota2 + nota3) / 3;
+        }
 
-  float mayorPromedio = 0;
-  int posicion;
+        float mayorPromedio = 0;
+        int posicion;
 
-  for (size_t i = 0; i < N_ESTUDIANTES; i++)
-  {
-    if (estudiantes[i].promedio > mayorPromedio)
-    {
-      mayorPromedio = estudiantes[i].promedio;
-      posicion = i;
-    }
-  }
+        for (size_t i = 0; i < N_ESTUDIANTES; i++)
+        {
+                if (estudiantes[i].promedio > mayorPromedio)
+                {
+                        mayorPromedio = estudiantes[i].promedio;
+                        posicion = i;
+                }
+        }
 
-  printf("\nEl estudiante #%d tiene el mejor promedio.\n", posicion + 1);
-  printf("Nombre: %s\n", estudiantes[posicion].nombre);
-  printf("Correo: %s\n", estudiantes[posicion].correo);
-  printf("Edad: %d\n", estudiantes[posicion].edad);
-  printf("Promedio: %.2f\n", estudiantes[posicion].promedio);
+        printf("\nEl estudiante #%d tiene el mejor promedio.\n", posicion + 1);
+        printf("Nombre: %s\n", estudiantes[posicion].nombre);
+        printf("Correo: %s\n", estudiantes[posicion].correo);
+        printf("Edad: %d\n", estudiantes[posicion].edad);
+        printf("Promedio: %.2f\n", estudiantes[posicion].promedio);
 
-  return 0;
+        return 0;
 }
