254ce082126b5739acd60176c185204df4ee4a4d
Jasmir Medina <201848984+fn-cafeina@users.noreply.github.com>
2026-03-13 08:04:43 -0600

    Nuevo proyecto

---
+++ POO/Proyecto_2/Program.cs
@@ -0,0 +1,15 @@
+﻿int dato1 = 20;
+int dato2 = 10;
+int resultado;
+
+resultado = dato1 + dato2;
+Console.WriteLine($"{dato1} + {dato2} = {resultado}");
+
+resultado = dato1 - dato2;
+Console.WriteLine($"{dato1} - {dato2} = {resultado}");
+
+resultado = dato1 * dato2;
+Console.WriteLine($"{dato1} * {dato2} = {resultado}");
+
+resultado = dato1 / dato2;
+Console.WriteLine($"{dato1} / {dato2} = {resultado}");
