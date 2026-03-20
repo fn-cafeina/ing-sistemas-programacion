6abbf3ba7239c6b648b02b910b0e324fa709538c
Jasmir Medina <201848984+fn-cafeina@users.noreply.github.com>
2026-03-19 19:33:43 -0600

    Nuevo proyecto

---
+++ POO/Proyecto_8/IMC/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+    2. Escribe un programa que calcule el Índice de Masa Corporal (IMC).
+    El usuario debe ingresar su peso en kilogramos y su altura en metros.
+    Usa double.TryParse para validar ambos datos. Luego, calcula el IMC con la
+    fórmula:
+
+    IMC = peso / altura^2
+
+    Clasifica el resultado como:
+
+    "Bajo peso" si el IMC < 18.5
+    "Normal" si está entre 18.5 y 24.9
+    "Sobrepeso" si está entre 25 y 29.9
+    "Obesidad" si es 30 o más
+*/
+
+Console.Clear();
+
+Console.Write("Ingrese su peso (kg): ");
+if (!double.TryParse(Console.ReadLine(), out double peso))
+{
+    Console.WriteLine("Error: Dato no válido.");
+    return;
+}
+
+Console.Write("Ingrese su altura (metros): ");
+if (!double.TryParse(Console.ReadLine(), out double altura))
+{
+    Console.WriteLine("Error: Dato no válido.");
+    return;
+}
+
+double IMC = peso / (altura * altura);
+
+string resultado = IMC switch
+{
+    >= 30 => "Obesidad",
+    >= 25 => "Sobrepreso",
+    >= 18.5 => "Normal",
+    _ => "Bajo peso"
+};
+
+Console.WriteLine($"\nSu IMC es: {IMC:F2}");
+Console.WriteLine($"Clasificación: {resultado}");
---
+++ POO/Proyecto_8/Temperatura/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+    1. Crea un programa que solicite al usuario ingresar una temperatura en
+    grados Celsius.
+
+    El programa debe validar que el dato ingresado sea numérico usando int.TryParse.
+    Luego, debe clasificar la temperatura como:
+
+    "Muy frío" si es menor a 10°C
+    "Frío" si está entre 10°C y 17°C
+    "Templado" si está entre 18°C y 25°C
+    "Caluroso" si está entre 26°C y 35°C
+    "Extremo" si es mayor a 35°C
+
+    Si el dato no es válido, debe mostrar un mensaje de error.
+*/
+
+Console.Clear();
+
+Console.Write("Ingrese la temperatura en grados celcius: ");
+if (!int.TryParse(Console.ReadLine(), out int C))
+{
+    Console.WriteLine("Error: Dato no válido.");
+    return;
+}
+
+string resultado = C switch
+{
+    > 35 => "Extremo",
+    >= 26 => "Caluroso",
+    >= 18 => "Templado",
+    >= 10 => "Frío",
+    _ => "Muy frío"
+};
+
+Console.WriteLine($"Temperatura: {resultado}");
