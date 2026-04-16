a22e163250a9681b81bfbdee8865fb2493011d4c
Jasmir Medina <201848984+fn-cafeina@users.noreply.github.com>
2026-04-15 21:24:26 -0600

    Nuevo ejercicio

---
+++ POO/Ejercicios_practicos/OverrideToString/Program.cs
@@ -0,0 +1,18 @@
+﻿Producto producto1 = new()
+{
+    Nombre = "Retornable",
+    Precio = 50
+};
+
+Console.WriteLine(producto1.ToString());
+
+class Producto
+{
+    public string? Nombre { get; set; }
+    public decimal Precio { get; set; }
+
+    public override string ToString()
+    {
+        return $"{Nombre} - Precio: {Precio:C}";
+    }
+}
