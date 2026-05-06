19ff432d417e1ea08149d9ed5dca20bd6f3b1feb
Jasmir Medina <201848984+fn-cafeina@users.noreply.github.com>
2026-05-06 08:40:17 -0600

    Nuevo ejercicio practico

---
+++ POO/Ejercicios_practicos/ModificacionesInterfaz/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Net.Mime;
+
+public interface IArchivable
+{
+    void Leer();
+    void Escribir();
+}
+
+public interface IImprimible
+{
+    void Imprimir();
+}
+
+public class Documento : IImprimible, IArchivable
+{
+    int contenido;
+
+    public Documento(int x1, int x2)
+    {
+        this.contenido = x1 + x2;
+    }
+
+    public void Imprimir()
+    {
+        Console.WriteLine($"La suma de los valores es: {contenido}");
+    }
+
+    public void Leer()
+    {
+        Console.WriteLine("Leyendo CONTENIDO desde el disco duro");
+    }
+
+    public void Escribir()
+    {
+        Console.WriteLine("Escribiendo CONTENIDO en disco duro");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.Clear();
+
+        Documento unDocumento = new Documento(5, 10);
+
+        unDocumento.Imprimir();
+        unDocumento.Leer();
+        unDocumento.Escribir();
+
+        Console.WriteLine("-----------------------------");
+
+        Console.WriteLine("Press any key to continue...");
+        Console.ReadKey();
+    }
+}
