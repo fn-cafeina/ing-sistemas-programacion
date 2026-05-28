e3820246a41773222b64c89ecd44955107fb85ae
Jasmir Medina <201848984+fn-cafeina@users.noreply.github.com>
2026-05-28 09:09:33 -0600

    Mover proyectos

--- POO/Practica_8/Delegados/obj/Debug/net10.0/Delegados.AssemblyInfo.cs
+++ POO/Practica_8/Delegados/obj/Debug/net10.0/Delegados.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Delegados")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1df9d1be7c2fe9bd5c19765cfe08876a8596ec98")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9facd914f8d6d433dd35e13360be20b9830e188")]
 [assembly: System.Reflection.AssemblyProductAttribute("Delegados")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Delegados")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
